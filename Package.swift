0fb28dc7804fadd8ce79e7561742a0d21760c649
Alejandro Ramirez Aldariz <627538+janodev@users.noreply.github.com>
2022-03-29 17:30:58 +0200

    Set docc version to at least 1.0.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "KeychainStatic", type: .static, targets: ["Keychain"])
     ],
     dependencies: [
-        .package(url: "git@github.com:apple/swift-docc-plugin.git", branch: "main")
+        .package(url: "git@github.com:apple/swift-docc-plugin.git", from: "1.0.0")
     ],
     targets: [
         .target(
